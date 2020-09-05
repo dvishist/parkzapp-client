4d878dc5dab2704ef4c6e20ea11160ba266792f6
dvishist <54056185+dvishist@users.noreply.github.com>
2020-09-06 01:26:53 +1000

    GET vehicles from server and show on vehicles page

--- screens/appScreens/VehicleScreen.js
+++ screens/appScreens/VehicleScreen.js
@@ -1,36 +1,66 @@
 import React, { useEffect } from 'react'
-import { Text, View, FlatList, Card, CardItem } from 'react-native'
+import { Text, View, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native'
 import axios from 'axios'
 import API_URL from '../../components/apiurl'
 import AsyncStorage from '@react-native-community/async-storage'
-import { Title } from 'react-native-paper'
 
-export default function VehicleScreen() {
+export default function VehicleScreen(props) {
     const [vehicles, setVehicles] = React.useState(null)
+    axios.defaults.baseURL = API_URL
+    axios.defaults.headers.common['Authorization'] = props.userToken
 
-    axios.defaults.baseURL = API_URL;
+    //load vehicles from server
     useEffect(() => {
         async function setup() {
             try {
-                const userToken = await AsyncStorage.getItem('userToken')
-                axios.defaults.headers.common['Authorization'] = userToken
                 const { data } = await axios.get('users/vehicles')
-                setVehicles(data)
-                console.log(vehicles)
+                const vehicleList = data.map(item => {
+                    return { item, key: data.indexOf(item) }
+                })
+                setVehicles(vehicleList)
             } catch (err) {
+                console.log(err)
             }
         }
         setup()
     }, [])
 
     return (
-        <View style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            flex: 1
-        }}>
+        <View style={styles.container}>
+            <Text style={{ alignSelf: 'center' }}>REGISTERED VEHICLES</Text>
+            <FlatList
+                data={vehicles}
+                renderItem={({ item }) => (
+                    <TouchableOpacity>
+                        <View style={{ ...styles.vehicleItem, backgroundColor: item.item.model == 'R8' ? 'green' : 'silver' }}>
+                            <Text style={styles.headingText}>{`${item.item.manufacturer} ${item.item.model}`.toUpperCase()}</Text>
+                            <Text >{item.item.idNumber}</Text>
+                            <Text >{`Registered on: ${new Date(item.item.createdAt).toDateString()}`}</Text>
+                        </View>
+                    </TouchableOpacity>
 
+                )}
+            />
         </View>
-
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15,
+        flex: 1
+    },
+    vehicleItem: {
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 8,
+        marginVertical: 5,
+        backgroundColor: 'silver',
+        borderRadius: 20
+    },
+    headingText: {
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+})
