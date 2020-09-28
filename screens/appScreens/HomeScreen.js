9d80945e5ceede1868f17d0db80b6126ade2a48a
dvishist <54056185+dvishist@users.noreply.github.com>
2020-09-28 19:57:28 +1000

    display parking lots as markers

--- screens/appScreens/HomeScreen.js
+++ screens/appScreens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 import MapView from 'react-native-maps'
 import * as Permissions from 'expo-permissions'
+import mapStyle from '../../components/mapstyle'
 
 import axios from 'axios'
 import API_URL from '../../components/apiurl'
@@ -9,11 +10,13 @@ import API_URL from '../../components/apiurl'
 
 export default function HomeScreen(props) {
 
+    const [parkingsNearby, setParkingsNearby] = React.useState([])
+
     axios.defaults.baseURL = API_URL
     axios.defaults.headers.common['Authorization'] = props.userToken
 
     //by default set location to Melbourne coordinates
-    const [locationState, setState] = React.useState({
+    const [locationState, setLocationState] = React.useState({
         latitude: -37.8136,
         longitude: 144.9631,
         latitudeDelta: 0.03,
@@ -28,8 +31,10 @@ export default function HomeScreen(props) {
                 await Permissions.askAsync(Permissions.LOCATION)
             }
             navigator.geolocation.getCurrentPosition(
-                ({ coords: { latitude, longitude } }) => {
-                    setState({ ...locationState, latitude, longitude })
+                async ({ coords: { latitude, longitude } }) => {
+                    setLocationState({ ...locationState, latitude, longitude })
+                    const { data } = await axios.post('/parkings/findNearby', { latitude, longitude })
+                    setParkingsNearby(data)
                 },
                 error => console.log(error)
             )
@@ -39,34 +44,18 @@ export default function HomeScreen(props) {
 
     }
 
-    async function getParkingsNearby() {
-        try {
-            const parkings = await axios.post('/parkings/findNearby', {
-                latitude: locationState.latitude,
-                longitude: locationState.longitude
-            })
-            parkings.data.forEach(item => {
-                console.log(`Name : ${item.parking.name}`)
-                console.log(`Address : ${JSON.stringify(item.parking.address)}`)
-                console.log(`Distance : ${item.distance / 1000}km`)
-                console.log('       ')
-            })
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
 
 
     useEffect(() => {
         getLocation()
-        getParkingsNearby()
+
     }, [])
 
     return (
         <MapView
             showsUserLocation
             style={styles.map}
+            customMapStyle={mapStyle}
             provider="google"
             region={{
                 latitude: locationState.latitude,
@@ -75,6 +64,27 @@ export default function HomeScreen(props) {
                 longitudeDelta: locationState.longitudeDelta
             }}
         >
+            {
+                parkingsNearby ?
+                    parkingsNearby.map(parking => {
+                        return (
+                            <MapView.Marker
+                                key={parkingsNearby.indexOf(parking)}
+                                coordinate={parking.parking.coordinates}
+                                title={parking.parking.name}
+                                description={`${parking.parking.address.streetNumber} ${parking.parking.address.streetName}, ${parking.parking.address.city} `}
+                            >
+                                <Image source={require('../../assets/marker.png')}
+                                    style={{ height: 40, width: 40 }}
+                                >
+
+                                </Image>
+
+                            </MapView.Marker>
+                        )
+                    })
+                    : null
+            }
 
         </MapView>
 
