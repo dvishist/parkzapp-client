23557f85fdd00cc78999c4374f78caa05a94e52b
dvishist <54056185+dvishist@users.noreply.github.com>
2020-10-07 18:05:22 +1100

    refactor milisecondsToTime script

--- components/milisecondsToTime.js
+++ components/milisecondsToTime.js
@@ -1,19 +1,13 @@
-const { NativeModules } = require("react-native")
-
 const milisecondsToTime = (timeInMiliseconds) => {
     let h = Math.floor(timeInMiliseconds / 1000 / 60 / 60)
     let m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60)
     let s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60)
-    h = doubleDigit(h)
-    m = doubleDigit(m)
-    s = doubleDigit(s)
-    return (`${h}:${m}:${s}`)
+    let outputString = ""
+    outputString += h > 0 ? String(h) + " hr " : ""
+    outputString += m > 0 ? String(m) + " min " : ""
+    outputString += (h === 0 && s > 0) ? String(s) + " sec" : ""
+    return (outputString)
 }
 
-const doubleDigit = (t) => (
-    String(t).length === 1 ?
-        '0' + t
-        : t
-)
 
 export default milisecondsToTime
